9f7922647cd82a8534f049642a7546c74d8bdacf
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-07 17:40:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment5._4._1
+{
+    internal class Program
+    {
+
+        static void NumberProblem(int n)
+        {
+            if (n == 0)
+                return;
+
+            NumberProblem(n / 10);
+
+            Console.Write(n % 10 + " ");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            NumberProblem(number);
+            Console.WriteLine();
+        }
+    }
+}
